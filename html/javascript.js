cf3d9b4453c692a7e77bde89df2f74538df6ddc3
samridhi481 <164517564+samridhi481@users.noreply.github.com>
2024-09-15 17:58:34 +0530

    Add files via upload

---
+++ html/javascript.js
@@ -0,0 +1,108 @@
+import { todayDeal } from "./todayDeal.js";
+
+let slideBtnLeft = document.getElementById("slide-btn-left");
+let slideBtnRight = document.getElementById("slide-btn-right");
+let imgItem = document.querySelectorAll(".image-item");
+
+console.log(imgItem.length - 1);
+
+let startSlider = 0;
+let endSlider = (imgItem.length - 1) * 100; // 700
+
+slideBtnLeft.addEventListener("click", handleLeftBtn);
+
+function handleLeftBtn() {
+    if (startSlider < 0) {
+        startSlider += 100;
+    }
+    imgItem.forEach((element) => {
+        element.style.transform = `translateX(${startSlider}%)`;
+    });
+}
+
+slideBtnRight.addEventListener("click", handleRightBtn);
+
+function handleRightBtn() {
+    if (startSlider > -endSlider + 100) {
+        startSlider -= 100;
+    }
+    imgItem.forEach((element) => {
+        element.style.transform = `translateX(${startSlider}%)`;
+    });
+}
+
+// Render automatically
+function renderSlideAuto() {
+    if (startSlider > -endSlider + 100) {
+        handleRightBtn();
+    } else {
+        startSlider = 0;
+        imgItem.forEach((element) => {
+            element.style.transform = `translateX(${startSlider}%)`;
+        });
+    }
+}
+
+setInterval(renderSlideAuto, 2000);
+
+/***** Sidebar navigation *****/
+const sidebarNavigationEl = document.getElementById("sidebar-container-navigation-id");
+const sidebarOpenNavigationEl = document.getElementById("open-nav-sidebar");
+const sidebarCloseNavigationEl = document.getElementById("sidebar-navigation-close");
+
+sidebarOpenNavigationEl.addEventListener("click", () => {
+    sidebarNavigationEl.classList.toggle("sidebar-show");
+});
+sidebarCloseNavigationEl.addEventListener("click", () => {
+    sidebarNavigationEl.classList.toggle("sidebar-show");
+});
+
+/***** Today deals *****/
+console.log(todayDeal);
+let todayDealProductListEl = document.querySelector(".today_deals_product_list");
+console.log(todayDealProductListEl);
+
+let todayDealProductHTML = "";
+
+let todayDeallength = todayDeal.length;
+
+for (let i = 0; i < todayDeallength; i++) {
+    todayDealProductHTML += `
+        <div class="today_deals_product_item">
+            <div class="todayDeals_product_image">
+                <img src=${todayDeal[i].img} />
+            </div>
+            <div class="discount_Container">
+                <a href="#">Up to ${todayDeal[i].discount}% off</a>
+                <a href="#">${todayDeal[i].DealOfDay}</a>
+            </div>
+            <p>${todayDeal[i].desc}</p>
+        </div>
+    `;
+}
+
+todayDealProductListEl.innerHTML = todayDealProductHTML;
+
+let today_deal_btn_prevEl = document.getElementById("today_deal_btn_prev");
+let today_deal_btn_nextEl = document.getElementById("today_deal_btn_next");
+let today_deals_product_itemEl = document.querySelectorAll(".today_deals_product_item");
+
+let startProduct = 0;
+
+today_deal_btn_prevEl.addEventListener("click", () => {
+    if (startProduct < 0) {
+        startProduct += 500;
+    }
+    today_deals_product_itemEl.forEach((el) => {
+        el.style.transform = `translateX(${startProduct}%)`;
+    });
+});
+
+today_deal_btn_nextEl.addEventListener("click", () => {
+    if (startProduct > -1500) {
+        startProduct -= 500;
+    }
+    today_deals_product_itemEl.forEach((el) => {
+        el.style.transform = `translateX(${startProduct}%)`;
+    });
+});
